ac3ef19ace4967b70ac9c925e1ce7cde2c73d52e
Nikita Kozik <kozik@internet.ru>
2021-05-06 19:15:56 +0300

    v2

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,11 +1,13 @@
+window.onload = () => {
+//dropdown
 function toggleDropdown() {
 
     const toggleButton = document.querySelector('.open-dropdown'),
-          toggleContainer = document.querySelector('.dropdown-container');
+          toggleContainer = document.querySelector('.dropdown');
 
     toggleButton.addEventListener('click', (e) => {
         toggleContainer.classList.toggle('show');
-        toggleButton.classList.toggle('active');
+        toggleButton.classList.add('active');
         e.preventDefault();
         e.stopPropagation();
     });
@@ -22,3 +24,40 @@ function toggleDropdown() {
 }
 
 toggleDropdown();
+
+//toggle mobile nav
+function toggleMobileNav() {
+    const toggleMobileNavButton = document.querySelector('.toggle-navigation'),
+          navigation = document.querySelector('.links');
+
+          toggleMobileNavButton.addEventListener('click', (e) => {
+            navigation.classList.toggle('show');
+            
+            e.preventDefault();
+        });
+
+        window.addEventListener('click', (e) => {
+            if (!navigation.contains(e.target) && !toggleMobileNavButton.contains(e.target)) {
+                navigation.classList.remove('show');
+                e.preventDefault();
+            }
+        });
+}
+
+toggleMobileNav();
+
+//crop card title 
+
+function cropTitle() {
+    const cardTitle = document.querySelectorAll('.card-title');
+
+    for(let i = 0; i < cardTitle.length; i++) {
+        if(cardTitle[i].innerText.length > 60) {
+            cardTitle[i].textContent =  cardTitle[i].innerText.substring(0,60) + '...';
+        }
+    }
+}
+
+cropTitle();
+
+};
